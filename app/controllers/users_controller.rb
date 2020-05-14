4a08d6c20d4cd20de190d36b5e25137ac164f2f3
quinqu <quinruby0@gmail.com>
2020-05-13 18:59:54 -0700

    some controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def index 
+    @user = User.all 
+  end 
+
+  def show 
+    @user = User.find_by(id: params[:id])
+    if @user.nil? 
+      head :not_found
+      return 
+    end 
+  end 
+
+
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,19 @@
 class VotesController < ApplicationController
+
+
+  def index 
+    #TODO add if statemnet if we need show for user votes 
+    #Same with Works 
+    #works is going to need to see all the users that voted for the work 
+    # users show page will show all the works they voted for 
+    #https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/08-rails/nested-routes.md
+
+    @votes = Votes.all  
+  end 
+
+
+
+
+
+
 end
