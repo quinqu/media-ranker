0b70e7811b39bf502de59b064fbef6d7266299cd
quinqu <quinruby0@gmail.com>
2020-05-15 10:20:25 -0700

    work model test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do 
+    it "has a creator" do 
+      movie = works(:movie)
+      expect(movie.creator).must_equal "Jordan Peele"
+    end 
+    it "can change the creator" do 
+      album = works(:album)
+      album.update_attribute(:creator, "Alex G")
+      expect(album.creator).must_equal "Alex G"
+    end 
+
+    it "will not update if there is a blank input" do 
+      work = Work.new()
+      saved = work.save
+      expect(saved).must_equal false
+    end 
+  end 
+
+
+
+
+
+
+
+
 end
