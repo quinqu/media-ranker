9dc2bffb7380e7cca115b43a1886d2de774cf2b2
quinqu <quinruby0@gmail.com>
2020-05-14 19:42:59 -0700

    partial for new and edit form for work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -20,11 +20,49 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
     if @work.save 
       redirect_to root_path
-      #redirect_to work_path(@passenger.id)
+      #redirect_to work_path(@work.id)
     else
       render :new
       return 
     end 
+  end 
+
+  def edit 
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return 
+    end
+
+  end 
+
+
+  def update 
+    @work = Work.find_by(id: params[:id])
+    if @work.nil? 
+      head :not_found
+      return 
+    elsif @work.update(work_params)
+      redirect_to work_path
+      return 
+    else
+      render :edit 
+      return 
+    end 
+
+
+  end 
+
+  def destroy 
+    @work = Work.find_by(id: params[:id])
+    if @work.nil? 
+      redirect_to works_path 
+      return 
+    end 
+
+    @work.destroy 
+    redirect_to works_path 
+    return 
   end 
 
   private 
