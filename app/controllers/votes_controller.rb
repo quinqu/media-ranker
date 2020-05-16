5b3c1e33e6eaecfe434d03a71278bd073bed572b
quinqu <quinruby0@gmail.com>
2020-05-16 15:23:49 -0700

    user needs to be signed in to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,7 +3,13 @@ class VotesController < ApplicationController
   def upvote 
     @vote = Vote.find_by(id: params[:format])
     user  = User.find_by(id: session[:user_id])
-    if User.not_voted(@vote.id, user.id)
+
+
+
+    if user.nil? 
+      flash[:error] = "You need to be logged in to vote on media"
+      redirect_to login_path
+    elsif User.not_voted(@vote.id, user.id)
       @vote.users << user
       redirect_to works_path
     else
