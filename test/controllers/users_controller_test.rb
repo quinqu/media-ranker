8044abda58ca70423eeb5f61f68165519789b4a1
quinqu <quinruby0@gmail.com>
2020-05-20 13:27:08 -0700

    dry up user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,50 +1,40 @@
 require "test_helper"
 
 describe UsersController do
+  let (:user_hash) {
+    { user: {
+        username: "Grace Hopper"
+      }
+    }
+  } 
 
   it "must get login_form" do
     get login_path
-
     must_respond_with :success
   end
 
   describe "logging in" do 
     it "can log in a new user" do 
-      
-      user_hash = {
-        user: {
-          username: "Grace Hopper"
-        }
-      }
-
      expect{
       post login_path, params: user_hash
      }.must_differ "User.count", 1
-     
+
       must_respond_with :redirect
       
-     user = User.find_by(username: user_hash[:user][:username])
+      user = User.find_by(username: user_hash[:user][:username])
 
       expect(user).wont_be_nil
-      #look up user by ID
-      #session is a hash, like params
       expect(session[:user_id]).must_equal user.id
       expect(user.username).must_equal user_hash[:user][:username]
     end 
 
     it "can log in an exising user" do 
-     
-      user_hash = {
-        user: {
-          username: "Quin Quintero"
-        }
-      }
-
       post login_path, params: user_hash
 
       expect {
         post login_path, params: user_hash
       }.wont_change "User.count"
+
       user = User.find_by(username: user_hash[:user][:username])
       expect(session[:user_id]).must_equal user.id
     end 
@@ -52,13 +42,7 @@ describe UsersController do
 
   describe "logout" do 
     it "can logout user" do 
-      user_hash = {
-        user: {
-          username: "Rie"
-        }
-      }
       post login_path, params: user_hash
-
       post logout_path
 
       expect(session[:user_id]).must_be_nil
@@ -68,14 +52,8 @@ describe UsersController do
 
   describe " current " do 
     it "will direct to current user" do 
-      user = User.create(username: "test user")
-      login_data = {
-        user: {
-          username: "test user"
-        }
-      }
-
-      post login_path, params: login_data
+      user = User.create(username: "Grace Hopper")
+      post login_path, params: user_hash
 
       expect(session[:user_id]).must_equal user.id
       get current_user_path
