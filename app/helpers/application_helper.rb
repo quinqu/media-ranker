6721c61bfd0300d0fc378419cc3b3e8d413646b0
quinqu <quinruby0@gmail.com>
2020-05-21 13:53:26 -0700

    application view dry up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def alert_style(flash)
+    if flash[:error]
+      return "flash-msg__section alert alert-danger"
+    elsif flash[:welcome]
+      return "flash-msg__section alert alert-success"
+    elsif flash[:notice]
+      return "flash-msg__section alert alert-info"
+    end 
+  end 
 end
